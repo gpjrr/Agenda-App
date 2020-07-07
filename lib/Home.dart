8ea5e562c0a1fbd735160e8e58b29a24ff6bdd05
unknown <erwin20rr@hotmail.com>
2020-07-06 23:02:17 -0500

    barra menu

--- lib/Home.dart
+++ lib/Home.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:agendaprocrastinacion/Task.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+//import 'package:flutter/cur';
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -11,6 +11,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Task> Tareas=new List();
+  int TabIndex=0;
   @override
   void initState(){
     super.initState();
@@ -20,211 +21,249 @@ class _HomeState extends State<Home> {
     Tareas=Tareas.isNotEmpty?Tareas:ModalRoute.of(context).settings.arguments;
     /*for(int i=0;i<Tareas.length;i++)
       print( " $i =  ${Tareas[i].Nombre}" );*/
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(211, 211, 211, 1) ,
-      appBar: AppBar(
-        iconTheme: IconThemeData( color: Colors.black ),
-        centerTitle: true,
-        title: Text(
-          'lista de tareas',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-            letterSpacing: 1,
-            fontFamily: 'Bellota-BoldItalic',
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(211, 211, 211, 1) ,
+        appBar: AppBar(
+          iconTheme: IconThemeData( color: Colors.black ),
+          centerTitle: true,
+          title: Text(
+            'lista de tareas',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+              letterSpacing: 1,
+              fontFamily: 'Bellota-BoldItalic',
+            ),
+          ),
+          backgroundColor: Colors.yellow[500],
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                child: Text(
+                  'Tareas',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    letterSpacing: 1,
+                    fontFamily: 'Bellota-BoldItalic',
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'Metas',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    letterSpacing: 1,
+                    fontFamily: 'Bellota-BoldItalic',
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        backgroundColor: Colors.yellow[500],
-      ),
 
-      body: SafeArea(
+        body: TabBarView(
+          children: <Widget>[
+            SafeArea(
 
-        child: ListView.builder(
-          itemCount: Tareas.length,
-          itemBuilder: (context,index){
-            return Dismissible(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 2,
-                  horizontal: 2,
-                ),
-                child: Card(
-                  //borderOnForeground: true,
-                  child: ListTile(
-                    onTap: ()async{
+              child: ListView.builder(
+                itemCount: Tareas.length,
+                itemBuilder: (context,index){
+                  return Dismissible(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 2,
+                        horizontal: 2,
+                      ),
+                      child: Card(
+                        //borderOnForeground: true,
+                        child: ListTile(
+                          onTap: ()async{
 
-                      dynamic cosa2=await Navigator.pushNamed(context,'/editor',arguments: {
-                        'Nombre': Tareas[index].Nombre ,
-                        'Descripcion': Tareas[index].Descripcion ,
-                        'Dias':Tareas[index].Dias,
-                        'Tempo':Tareas[index].Tempo,
-                        'Hora':Tareas[index].Hora,
-                        'Minu':Tareas[index].Minu,
-                        'Today':Tareas[index].Today
-                      } );
-                      //print(cosa2);
-                      setState( (){
+                            dynamic cosa2=await Navigator.pushNamed(context,'/editor',arguments: {
+                              'Nombre': Tareas[index].Nombre ,
+                              'Descripcion': Tareas[index].Descripcion ,
+                              'Dias':Tareas[index].Dias,
+                              'Tempo':Tareas[index].Tempo,
+                              'Hora':Tareas[index].Hora,
+                              'Minu':Tareas[index].Minu,
+                              'Today':Tareas[index].Today
+                            } );
+                            //print(cosa2);
+                            setState( (){
 
-                        Task Sujeto=Task(
-                          Nombre: cosa2['Nombre'],
-                          Limpio: true,
-                          Descripcion: cosa2['Descripcion'],
-                          Dias:  cosa2['Dias'] ,
-                          Tempo: cosa2['Tempo'],
-                          Lave: UniqueKey(),
-                          Today: cosa2['Today'],
-                          Hora: cosa2['Hora'],
-                          Minu: cosa2['Minu'],
-                        );
-                        print("H=${Sujeto.Hora} M=${Sujeto.Minu}  ");
-                        Tareas[index]=Sujeto;
+                              Task Sujeto=Task(
+                                Nombre: cosa2['Nombre'],
+                                Limpio: true,
+                                Descripcion: cosa2['Descripcion'],
+                                Dias:  cosa2['Dias'] ,
+                                Tempo: cosa2['Tempo'],
+                                Lave: UniqueKey(),
+                                Today: cosa2['Today'],
+                                Hora: cosa2['Hora'],
+                                Minu: cosa2['Minu'],
+                              );
+                              print("H=${Sujeto.Hora} M=${Sujeto.Minu}  ");
+                              Tareas[index]=Sujeto;
+                            });
+
+
+                          },
+                          leading: Icon( Icons.people ),
+                          title: Text(
+                            Tareas[index].Nombre.substring(0, Tareas[index].Nombre.length>=18?18:Tareas[index].Nombre.length  ),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 25,
+                              letterSpacing: 1,
+                              fontFamily: 'EastSeaDokdo-Regular',
+                              //fontFamily: 'Caveat-Regular',
+                            ),
+                          ),
+                          trailing: Icon(
+                            Icons.done,
+                            size: 40,
+                            color: Colors.green,
+                          ),
+
+                        ),
+                      ),
+                    ),
+                    key: Tareas[index].Lave,
+                    secondaryBackground: Container(
+                      color: Colors.red,
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+
+                          Icons.delete,
+                          color: Colors.black,
+                          size: 40,
+                        ),
+                      ),
+                    ),
+                    background: Container(
+                      color: Colors.green,
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+
+                          Icons.flag,
+                          color: Colors.black,
+                          size: 40,
+                        ),
+                      ),
+                    ),
+                    onDismissed: ( direction ) async{
+                      if(  direction==DismissDirection.startToEnd ) {
+                        dynamic work = await Navigator.pushNamed(
+                            context, '/tiempo', arguments: {
+                          'Hora': Tareas[index].Hora,
+                          'Minu': Tareas[index].Minu,
+                        });
+                      }
+
+                      setState(() {
+                        /*
+                      print("tam== ${Tareas.length} I=$index");
+                      print("${Tareas[index].Tempo}");
+                      print("${Tareas[index].Hora} ${Tareas[index].Minu}");
+                      */
+                        Tareas.removeAt(index);
                       });
 
-
                     },
-                    leading: Icon( Icons.people ),
-                    title: Text(
-                        Tareas[index].Nombre.substring(0, Tareas[index].Nombre.length>=18?18:Tareas[index].Nombre.length  ),
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 25,
-                          letterSpacing: 1,
-                          fontFamily: 'EastSeaDokdo-Regular',
-                          //fontFamily: 'Caveat-Regular',
-                        ),
-                    ),
-                    trailing: Icon(
-                      Icons.done,
-                      size: 40,
-                      color: Colors.green,
-                    ),
+                  );
+                },
 
-                  ),
-                ),
-              ),
-              key: Tareas[index].Lave,
-              secondaryBackground: Container(
-                color: Colors.red,
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Icon(
-
-                    Icons.delete,
-                    color: Colors.black,
-                    size: 40,
-                  ),
-                ),
-              ),
-              background: Container(
-                color: Colors.green,
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Icon(
-
-                    Icons.flag,
-                    color: Colors.black,
-                    size: 40,
-                  ),
-                ),
-              ),
-              onDismissed: ( direction ) async{
-                if(  direction==DismissDirection.startToEnd ) {
-                  dynamic work = await Navigator.pushNamed(
-                      context, '/tiempo', arguments: {
-                    'Hora': Tareas[index].Hora,
-                    'Minu': Tareas[index].Minu,
-                  });
-                }
-
-                  setState(() {
-                    /*
-                    print("tam== ${Tareas.length} I=$index");
-                    print("${Tareas[index].Tempo}");
-                    print("${Tareas[index].Hora} ${Tareas[index].Minu}");
-                    */
-                    Tareas.removeAt(index);
-                  });
-
-                  },
-            );
-          },
-
-        ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async{
-          Task Nuevo=Task(
-            Nombre: '',
-            Descripcion: "",
-            Limpio: true,
-            Hora: "00",
-            Minu: "00",
-            Dias: List.filled(7, false),
-            Lave: UniqueKey(),
-            Tempo: false,
-            Today:true,
-          );
-          ///print("=-=${Nuevo.Tempo}");
-          Nuevo.Hoy();
-          dynamic cosa=await Navigator.pushNamed(context,'/editor',arguments: {
-            'Nombre': Nuevo.Nombre ,
-            'Descripcion': Nuevo.Descripcion ,
-            'Dias':Nuevo.Dias,
-            'Tempo':Nuevo.Tempo,
-            'Hora':Nuevo.Hora,
-            'Minu':Nuevo.Minu,
-            'Today':Nuevo.Today,
-          } );
-          setState( (){
-            if( cosa!=null ) {
-              Task Sujeto = Task(
-                Nombre: cosa['Nombre'],
-                Limpio: true,
-                Descripcion: cosa['Descripcion'],
-                Dias: cosa['Dias'],
-                Tempo: cosa['Tempo'],
-                Lave: UniqueKey(),
-                Today: cosa['Today'],
-                Hora: cosa['Hora'],
-                Minu: cosa['Minu'],
-              );
-              Tareas.add(Sujeto);
-            }
-          });
-        },
-        backgroundColor: Colors.blue[700],
-        child: Icon(
-          Icons.add,
-          color: Colors.black87,
-        ),
-      ),
-
-      drawer: Drawer(
-
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text('Tips'),
-              trailing: Icon(
-                Icons.lightbulb_outline,
-                color: Colors.yellow,
               ),
             ),
-            Divider(),
-            ListTile(
-              title: Text('Records'),
-              trailing: Icon(
-                Icons.book,
-                color: Colors.brown,
+            SafeArea(
+              child: Container(
+                color: Colors.blue,
               ),
-              onTap: (){
-                Navigator.pushNamed(context, '/TipList'  );
-              },
-            ),
+            )
           ],
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async{
+            Task Nuevo=Task(
+              Nombre: '',
+              Descripcion: "",
+              Limpio: true,
+              Hora: "00",
+              Minu: "00",
+              Dias: List.filled(7, false),
+              Lave: UniqueKey(),
+              Tempo: false,
+              Today:true,
+            );
+            ///print("=-=${Nuevo.Tempo}");
+            Nuevo.Hoy();
+            dynamic cosa=await Navigator.pushNamed(context,'/editor',arguments: {
+              'Nombre': Nuevo.Nombre ,
+              'Descripcion': Nuevo.Descripcion ,
+              'Dias':Nuevo.Dias,
+              'Tempo':Nuevo.Tempo,
+              'Hora':Nuevo.Hora,
+              'Minu':Nuevo.Minu,
+              'Today':Nuevo.Today,
+            } );
+            setState( (){
+              if( cosa!=null ) {
+                Task Sujeto = Task(
+                  Nombre: cosa['Nombre'],
+                  Limpio: true,
+                  Descripcion: cosa['Descripcion'],
+                  Dias: cosa['Dias'],
+                  Tempo: cosa['Tempo'],
+                  Lave: UniqueKey(),
+                  Today: cosa['Today'],
+                  Hora: cosa['Hora'],
+                  Minu: cosa['Minu'],
+                );
+                Tareas.add(Sujeto);
+              }
+            });
+          },
+          backgroundColor: Colors.blue[700],
+          child: Icon(
+            Icons.add,
+            color: Colors.black87,
+          ),
+        ),
+
+        drawer: Drawer(
+
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text('Tips'),
+                trailing: Icon(
+                  Icons.lightbulb_outline,
+                  color: Colors.yellow,
+                ),
+              ),
+              Divider(),
+              ListTile(
+                title: Text('Records'),
+                trailing: Icon(
+                  Icons.book,
+                  color: Colors.brown,
+                ),
+                onTap: (){
+                  Navigator.pushNamed(context, '/TipList'  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
